0a081608728b8659ae72c21583d23a05e9280918
Syprose Gwako <syprosegwako@gmail.com>
2019-11-25 13:57:19 +0300

    added driver head

---
+++ includes/driverhead.php
@@ -0,0 +1,52 @@
+
+<!doctype html>
+<html class="no-js h-100" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Traffic Offenders Registration System</title>
+    <meta name="description" content="A high-quality &amp; free Bootstrap admin dashboard template pack that comes with lots of templates and components.">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <link href="../googlefonts/icon.css" rel="stylesheet">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../pages/styles/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.1.0/material.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.material.min.css">
+
+ 
+    <link rel="stylesheet" id="main-stylesheet" data-version="1.1.0" href="styles/shards-dashboards.1.1.0.min.css">
+    <link rel="stylesheet" href="styles/extras.1.1.0.min.css">
+    <link rel="stylesheet" type="text/css" href="../datatables/DataTables-1.10.18/css/jquery.dataTables.css">
+    <link rel="stylesheet" type="text/css" href="../pages/styles/table.css">
+    <link rel="stylesheet" type="text/css" href="../pages/styles/update.css">
+
+    <!--  scripts -->
+    <script type="text/javascript" src="../js/jquery/jquery.min.js"></script>
+    <script src="../js/popper.min.js"></script>
+    <script type="text/javascript" src="../js/jquery/jquery.js"></script>
+    <script src='../fontawesome/js/all.js'></script>
+    <script src="../js/sweetalert.min.js"></script>
+    <script async defer src="../js/buttons.js"></script>
+    <script src="../datatables/jQuery-3.3.1/jquery-3.3.1.js" type="text/javascript"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.material.min.js"></script>
+   
+
+    <style>
+        .main-sidebar .nav .nav-item .nav-link {
+            border-bottom: 0px;
+        }
+
+        .main-sidebar .nav .nav-item .nav-link:hover {
+            box-shadow: none;
+            background: transparent !important;
+            color: #007bff;
+        }
+
+    </style>
+</head>
